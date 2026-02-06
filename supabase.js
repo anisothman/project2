0215aa29c552d6bf0c29fed454cf8fc766d12cf4
othman anis <oanis052@gmail.com>
2026-02-06 20:23:21 +0100

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,9 @@
+// supabase.js
+
+const supabaseUrl = "https://gnonstirbsopegqyesvn.supabase.co";
+const supabaseKey = "sb_publishable_hcHJlF7YAK6TmlhyDYioFQ_u6dO8_K4";
+
+window.supabaseClient = supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
